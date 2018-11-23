98d3e0ed43151cbeed5d53e12c421d6c6371316d
mengbenben <1286108812@qq.com>
2018-11-23 17:03:42 +0800

    11.22

---
+++ src/components/demo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="demo">{{ msg }}</div>
+</template>
+
+<script>
+    export default {
+        name: "demo",
+        data (){
+            return {
+                msg: 'SPA demo'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .demo {
+        color: red;
+    }
+</style>
